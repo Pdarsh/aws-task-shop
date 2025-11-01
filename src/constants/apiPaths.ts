e3dcc8b0e1e0d8eff0c76d152c4748af7834cac7
Darsh Patel <47714601+Pdarsh@users.noreply.github.com>
2025-11-01 15:54:14 +0530

    Add files via upload

---
+++ src/constants/apiPaths.ts
@@ -0,0 +1,9 @@
+const API_PATHS = {
+  product: "http://ec2-13-232-129-125.ap-south-1.compute.amazonaws.com/",
+  order: "http://ec2-13-232-129-125.ap-south-1.compute.amazonaws.com/",
+  import: "http://ec2-13-232-129-125.ap-south-1.compute.amazonaws.com/",
+  bff: "http://ec2-13-232-129-125.ap-south-1.compute.amazonaws.com/",
+  cart: "http://ec2-13-232-129-125.ap-south-1.compute.amazonaws.com/",
+};
+
+export default API_PATHS;
